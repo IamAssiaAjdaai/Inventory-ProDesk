6601d9cfaf6c6b30bbe56cde906d36488cc6fd5c
Assia Ajdaai <assixa.chitanixa@gmail.com>
2024-11-25 08:18:36 +0100

    Login Form with database cnx

---
+++ Inventory ProDesk/MainForm.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Inventory_ProDesk
+{
+    /// <summary>
+    /// Interaction logic for MainForm.xaml
+    /// </summary>
+    public partial class MainForm : Window
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+        }
+    }
+}
--- Inventory ProDesk/MainWindow.xaml.cs
+++ Inventory ProDesk/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
+﻿using Microsoft.Data.SqlClient;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Inventory_ProDesk
 {
@@ -20,5 +14,98 @@ namespace Inventory_ProDesk
         {
             InitializeComponent();
         }
+
+        private void InputTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            HandlePlaceholderFocus(InputTextBox, PlaceholderLabel, "Enter your username");
+            HandlePlaceholderFocus(passwordTextBox, passwordPlaceholderLabel, "Enter your password");
+        }
+
+        private void InputTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            HandlePlaceholderLostFocus(InputTextBox, PlaceholderLabel, "Enter your username");
+            HandlePlaceholderLostFocus(passwordTextBox, passwordPlaceholderLabel, "Enter your password");
+        }
+
+        private void InputTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(InputTextBox.Text))
+            {
+                PlaceholderLabel.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = InputTextBox.Text;
+            string password = "Enter your password";
+
+            if (ValidateLogin(username, password))
+            {
+                MainForm mainForm = new MainForm();
+                MainWindow window = new MainWindow();
+                mainForm.Show();
+                window.Close();
+            }
+            else
+            {
+                ShowError("Invalid username or password.");
+            }
+        }
+        #region Helper
+
+        private void HandlePlaceholderFocus(TextBox textBox, Label placeholderLabel, string defaultText)
+        {
+            if (textBox.Text == defaultText)
+            {
+                placeholderLabel.Content = defaultText;
+                placeholderLabel.Visibility = Visibility.Visible;
+                textBox.Text = string.Empty;
+            }
+        }
+
+
+        private void HandlePlaceholderLostFocus(TextBox textBox, Label placeholderLabel, string defaultText)
+        {
+            if (string.IsNullOrEmpty(textBox.Text))
+            {
+                textBox.Text = defaultText;
+                placeholderLabel.Visibility = Visibility.Collapsed;
+            }
+        }
+
+
+        private void ShowError(string errorMessage)
+        {
+            InputTextBox.BorderBrush = new SolidColorBrush(Colors.Red);
+            passwordTextBox.BorderBrush = new SolidColorBrush(Colors.Red);
+            ErrorPlaceholderLabel.Content = errorMessage;
+        }
+
+        private bool ValidateLogin(string username, string password)
+        {
+            string cnx = "Data Source=DESKTOP-4DM0DVG\\SQLEXPRESS;Initial Catalog=InventoryProDeskDB;Integrated Security=True;Trust Server Certificate=True";
+            string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password = @Password";
+            using(SqlConnection connection = new SqlConnection(cnx)) 
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlCommand cmd = new SqlCommand(query,connection))
+                    {
+                        cmd.Parameters.AddWithValue("@Username", username);
+                        cmd.Parameters.AddWithValue("@Password", password);
+                        int result = (int)cmd.ExecuteScalar();
+                        return result > 0;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error connecting to database: " + ex.Message);
+                    return false;
+                }    
+            }
+        }
+        #endregion
     }
 }
